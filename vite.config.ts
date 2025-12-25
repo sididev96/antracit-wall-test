80cebb8c8b93e887ba8359716ad75caf5948c184
Sejjid Gashi <sejjid.gashi@gmail.com>
2025-12-25 16:52:01 +0100

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/antracit-wall-test/',
   server: {
     host: "::",
     port: 8080,
